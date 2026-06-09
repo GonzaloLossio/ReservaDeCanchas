c3b00ae547328601aa17680aa0fd21d71cea0e7e
GonzaloLossio <gonzalolozioqwer@gmail.com>
2026-06-09 17:26:24 -0500

    feat: configuracion base de FastApi

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str 
+    debug: bool = False
+    database_url: str 
+    secret_key: str
+    algorithm: str 
+    access_token_expire_minutes: int = 30
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()        
